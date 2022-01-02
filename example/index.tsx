8e55e2bbd806611e2f00193b78e5c2d51e97fefd
Jonathan Newman <jbnewm@gmail.com>
2022-01-02 02:31:57 -0600

    fixed tests

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Prompt } from '../src/components/Prompt';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Prompt />
     </div>
   );
 };
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Prompt } from '../stories/Prompt.stories';
 
-describe('Thing', () => {
+describe('Prompt', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Prompt />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
