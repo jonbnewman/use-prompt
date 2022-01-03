d718b3816983f6bb258f09602742169889796b7f
Jonathan Newman <jbnewm@gmail.com>
2022-01-03 14:39:46 -0600

    removed unnecessary import

--- src/usePrompt.tsx
+++ src/usePrompt.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 export type PromptResponse = any;
 export type RendererCallback = (prompt: {
