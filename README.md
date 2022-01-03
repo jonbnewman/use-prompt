058d9cf8892ad3c9036a8000e9849e95638b7ae4
Jonathan Newman <jbnewm@gmail.com>
2022-01-03 11:06:03 -0600

    added license badge and fixed tests

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # use-prompt
 
 ![main workflow](https://github.com/jonbnewman/use-prompt/actions/workflows/main.yml/badge.svg)
+![MIT License](https://img.shields.io/github/license/jonbnewman/use-prompt)
 
 **use-prompt** is a library that lets you conveniently display a React component to a user asynchronously.
 
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -5,7 +5,7 @@ import { StandardModalPrompt } from '../stories/Prompter.stories';
 describe('Modal Prompt', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<StandardModalPrompt />, div);
+    ReactDOM.render(<StandardModalPrompt component="standard" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
