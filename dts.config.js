f284c737f6672bb5e49e10dba63a636200fe53aa
Jonathan Newman <jbnewm@gmail.com>
2022-01-04 15:09:19 -0600

    cleaned up dts config

--- dts.config.js
+++ dts.config.js
@@ -17,10 +17,6 @@ module.exports = {
         extract: !!options.writeMeta,
       })
     );
-
-    console.info('options.format', options.format);
-    // options.format = '';
-
     return config;
   },
 };
